21225e806b799af7027d344186bf7a996e5b54fd
Rick42Morty <evgenevgen00@mail.ru>
2018-10-04 23:41:12 +0300

    change concept

--- src/index.js
+++ src/index.js
@@ -1,54 +1,45 @@
 module.exports = function count(s, pairs) {
-  // your implementation
   let N = 1;
   let lenPairs = pairs.length;
+  let lenS = s.length;
 
   if (pairs[0][1] > 40) return 0;
-  if (pairs.length >8) return 0;
+  if (pairs.length > 8) return 0;
   if (s.length > 5) return 0;
 
   for (let i = 0; i < lenPairs; i++){
     N *= Math.pow(pairs[i][0], pairs[i][1]);
   }
 
+  let maxPrime = 0;
+  let product = 1;
 
-  let count = 0;
+  for (i = 0; i < lenPairs; i++){
+    if (pairs[i][0] > maxPrime) {maxPrime = pairs[i][0];}
+    product *= pairs[i][0];
+  }
 
-  let lenS = s.length;
+  let s1 = "01";
 
   outer:
-  for (let k = 0; k < N; k++){
-    for (let j = 0; j < lenS; j++){
-      if (s[j] == 1 && GCD(k+j, N) != 1) continue outer;
-      if (s[j] == 0 && GCD(k+j, N) == 1) continue outer;
+  for (let j = 2; j < product; j++){
+    for (i = 0; i < lenPairs; i++){
+      if (j % pairs[i][0] == 0){
+        s1 += "0";
+        continue outer;
+      }
     }
-    count++;
+    s1 += "1";
   }
 
-  return count;
-}
-
-//функция для нахождения НОД двух чисел
-
-function GCD (b, a) {
-  if (a == b) return a;
-
-  let min = 0;
-  let max = 0;
-
-  if (a > b) {
-    max = a;
-    min = b;
-  } else {
-    let min = a;
-    let max = b;
+  let counter = 0;
+  let pos = s1.indexOf(s);
+  while (pos !== -1) {
+    counter++;
+    pos = s1.indexOf(s, pos + 1);
   }
 
-  let temp = 0;
-  while (min != 0) {
-    temp = min;
-    min = max % min;
-    max = temp;
-  }
-  return max;
+  let ans = counter*N/product;
+
+  return ans;
 }
