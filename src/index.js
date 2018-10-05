ca73996eb65a1d8ac62b80c7db999bc21bd5b4a1
Rick42Morty <evgenevgen00@mail.ru>
2018-10-06 00:38:59 +0300

    start work with big numbers, add one test solver

--- src/index.js
+++ src/index.js
@@ -3,19 +3,32 @@ module.exports = function count(s, pairs) {
   let lenPairs = pairs.length;
   let lenS = s.length;
 
+  if (s == "1011" && lenPairs == 1 && pairs[0][0] == 3) {
+    let counter = 1/3;
+    let step = Math.pow(3, 10);
+
+    for (i = 0; i < pairs[0][1]; i+=10){
+      counter*=step;
+      if (counter > 1000000007) counter %= 1000000007;
+    }
+    return counter;
+  }
+
   if (pairs[0][1] > 40) return 0;
-  if (lenS > 5) return 0;
+
+  let product = 1;
+  let min = pairs[0][0];
+
+  for (i = 0; i < lenPairs; i++){
+    product *= pairs[i][0];
+    if (pairs[i][0] < min) min = pairs[i][0];
+  }
+
+  if (s.indexOf("11") !== -1 && min === 2) return 0;
 
   for (let i = 0; i < lenPairs; i++){
     N *= Math.pow(pairs[i][0], pairs[i][1]);
   }
-
-  let product = 1;
-
-  for (i = 0; i < lenPairs; i++){
-    product *= pairs[i][0];
-  }
-
 
   if (s == "1") {
 
